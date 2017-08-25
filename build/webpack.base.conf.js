f2d5ee553aeda761f139c1ef163c53068c333647
jveguo <jveguo16@gmail.com>
2017-08-26 01:10:08 +0800

    webpack cofig

--- build/webpack.base.conf.js
+++ build/webpack.base.conf.js
@@ -41,10 +41,10 @@ module.exports = {
         loader: 'vue-loader',
         options: vueLoaderConfig
       },
-      {
-        test: /\.less$/,
-        loader: "style-loader!css-loader!less-loader"
-      },
+      // {
+      //   test: /\.less$/,
+      //   loader: "style-loader!css-loader!less-loader"
+      // },
       {
         test: /\.js$/,
         loader: 'babel-loader',
