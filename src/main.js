ae3db7010de1870a21f691a823ea912602ba2199
jveguo <jveguo16@gmail.com>
2017-08-26 01:12:59 +0800

    import global less

--- src/main.js
+++ src/main.js
@@ -3,25 +3,8 @@
 import Vue from 'vue';
 import App from './App';
 import router from './router';
-// import VueRouter from 'vue-router';
 
-// import foods from '@/components/foods/foods';
-// import ratings from '@/components/ratings/ratings';
-// import seller from '@/components/seller/seller';
-
-// Vue.use(VueRouter);
-
-// Vue.config.productionTip = false;
-// const routes = [
-//   { path: '/', redirect: '/foods' },
-//   { path: '/foods', component: foods },
-//   { path: '/ratings', component: ratings },
-//   { path: '/seller', component: seller }
-// ];
-
-// const router = new VueRouter({
-//   routes // （缩写）相当于 routes: routes
-// });
+import './common/less/index.less';
 
 /* eslint-disable no-new */
 new Vue({
