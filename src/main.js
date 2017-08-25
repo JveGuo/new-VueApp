7afc488cc00b9c7be1d2599af0dad65fab4ba63b
jveguo <jveguo16@gmail.com>
2017-08-25 22:55:09 +0800

    add router

--- src/main.js
+++ src/main.js
@@ -2,26 +2,26 @@
 // (runtime-only or standalone) has been set in webpack.base.conf with an alias.
 import Vue from 'vue';
 import App from './App';
-// import router from './router';
-import VueRouter from 'vue-router';
+import router from './router';
+// import VueRouter from 'vue-router';
 
-import foods from '@/components/foods/foods';
-import ratings from '@/components/ratings/ratings';
-import seller from '@/components/seller/seller';
+// import foods from '@/components/foods/foods';
+// import ratings from '@/components/ratings/ratings';
+// import seller from '@/components/seller/seller';
 
-Vue.use(VueRouter);
+// Vue.use(VueRouter);
 
-Vue.config.productionTip = false;
-const routes = [
-  { path: '/', redirect: '/foods' },
-  { path: '/foods', component: foods },
-  { path: '/ratings', component: ratings },
-  { path: '/seller', component: seller }
-];
+// Vue.config.productionTip = false;
+// const routes = [
+//   { path: '/', redirect: '/foods' },
+//   { path: '/foods', component: foods },
+//   { path: '/ratings', component: ratings },
+//   { path: '/seller', component: seller }
+// ];
 
-const router = new VueRouter({
-  routes // （缩写）相当于 routes: routes
-});
+// const router = new VueRouter({
+//   routes // （缩写）相当于 routes: routes
+// });
 
 /* eslint-disable no-new */
 new Vue({
--- src/router/index.js
+++ src/router/index.js
@@ -3,13 +3,20 @@ import Router from 'vue-router';
 // import Hello from '@/components/Hello'
 
 Vue.use(Router);
+import foods from '@/components/foods/foods';
+import ratings from '@/components/ratings/ratings';
+import seller from '@/components/seller/seller';
+
+// Vue.use(VueRouter);
+
+Vue.config.productionTip = false;
+const routes = [
+  { path: '/', redirect: '/foods' },
+  { path: '/foods', component: foods },
+  { path: '/ratings', component: ratings },
+  { path: '/seller', component: seller }
+];
 
 export default new Router({
-  // routes: [
-  //   {
-  //     path: '/',
-  //     name: 'Hello',
-  //     component: Hello
-  //   }
-  // ]
+  routes
 });
