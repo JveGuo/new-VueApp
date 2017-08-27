1993009ecfa960def4b60f0b083562d7d6736ba0
jveguo <jveguo16@gmail.com>
2017-08-27 15:53:13 +0800

    change vue router

--- src/router/index.js
+++ src/router/index.js
@@ -1,13 +1,13 @@
 import Vue from 'vue';
 import Router from 'vue-router';
+import vueResource from 'vue-resource';
 // import Hello from '@/components/Hello'
 import foods from '@/components/foods/foods';
 import ratings from '@/components/ratings/ratings';
 import seller from '@/components/seller/seller';
 
 Vue.use(Router);
-
-// Vue.use(VueRouter);
+Vue.use(vueResource);
 
 Vue.config.productionTip = false;
 const routes = [
