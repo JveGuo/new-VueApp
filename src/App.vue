9bdc0efed8ade65fb34b5dc5b3dcd9061ba409c0
jveguo <jveguo16@gmail.com>
2017-08-27 15:53:59 +0800

    give data to header components

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,6 @@
 <template>
 	<div id="app">
-		<vheader></vheader>
+		<vheader v-bind:seller="seller"></vheader>
 		<div class="tab border-1px">
 			<div class="item-tab">
 				<router-link to="/foods">商品</router-link>
@@ -20,6 +20,20 @@
 //	import header from 'components/header/header';
 import header from '@/components/header/header';
 export default {
+	data () {
+		return {
+			seller: {}
+		};
+	},
+	created: function () {
+		this.$http.get('/api/seller').then((res) => {
+			res = res.body;
+			if (res.errorRet === 0) {
+				this.seller = res.data;
+				console.log(res.data);
+			}
+		});
+	},
 	components: {
 		'vheader': header
 	}
@@ -27,26 +41,26 @@ export default {
 </script>
 
 <style lang="less">
-	@import './common/less/common';
+@import './common/less/mixin';
 
-	@tab-height: 40px; //tab 高度
-	#app {
+@tab-height: 40px; //tab 高度
+#app {
+	width: 100%;
+	.tab {
+		display: flex;
 		width: 100%;
-		.tab {
-			display: flex;
-			width: 100%;
-			height: @tab-height;
-			line-height: @tab-height;
-			.border-1px(rgba(7,17,27,0.1));
-			.item-tab {
-				flex: 1;
-				text-align: center;
-				font-size: 14px;
-				color: rgb(77, 85, 93);
-				.active {
-					color: rgb(240, 20, 20)
-				}
+		height: @tab-height;
+		line-height: @tab-height;
+		.border-1px(rgba(7, 17, 27, 0.1));
+		.item-tab {
+			flex: 1;
+			text-align: center;
+			font-size: 14px;
+			color: rgb(77, 85, 93);
+			.active {
+				color: rgb(240, 20, 20)
 			}
 		}
 	}
+}
 </style>
