c52776fbc2476fb158a819699dc632c17c9b7367
jveguo <jveguo16@gmail.com>
2017-08-28 08:43:10 +0800

    add foods components

--- src/components/foods/foods.vue
+++ src/components/foods/foods.vue
@@ -1,10 +1,49 @@
 <template>
-  <div>食品</div>
+  <div class="foods">
+    <div class="menu-wrapper"></div>
+    <div class="foods-wrapper"></div>
+  </div>
 </template>
 
 <script>
-export default {};
+export default {
+  props: {
+    seller: {
+      type: Object
+    }
+  },
+  data () {
+    return {
+      foods: []
+    };
+  },
+  created () {
+    this.$http.get('/api/foods').then((res) => {
+      res = res.body;
+      if (res.errorRet === 0) {
+        this.foods = res.data;
+        console.log(this.foods);
+      }
+    });
+  }
+};
 </script>
 
 <style lang="less">
+  .foods{
+    display: flex;
+    position: absolute;
+    top: 174px;
+    bottom: 46px;
+    width: 100%;
+    overflow: hidden;
+    .menu-wrapper{
+      flex: 0 0 80px;
+      width: 80px;
+      background: #f3f5f7;
+    }
+    .foods-wrapper{
+      flex: 1;
+    }
+  }
 </style>
